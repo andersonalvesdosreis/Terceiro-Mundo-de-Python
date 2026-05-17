f44ee468a46efae783b5ae767f7899ecce1be223
Anderson Alves <anderson.filho.xap@gmail.com>
2026-05-17 12:00:07 -0500

    Adicionei resposta

---
+++ 074.py
@@ -0,0 +1,9 @@
+import random
+lista_1_10 = [1,2,3,4,5,6,7,8,9,10]
+numero_maior = 0
+lista_aleatoria = (random.choice(lista_1_10),random.choice(lista_1_10),random.choice(lista_1_10),random.choice(lista_1_10))
+print('Escolha do computador:')
+print(lista_aleatoria)
+for verificar_numero_maior in lista_aleatoria:
+    numero_maior = verificar_numero_maior
+    
