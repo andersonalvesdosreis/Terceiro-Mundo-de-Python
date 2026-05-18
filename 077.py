aac1909967291a0eee79e80d3cf8a1b3392dec98
Anderson Alves <anderson.filho.xap@gmail.com>
2026-05-18 12:14:46 -0500

    Adicionei Resposta

--- 077.py
+++ 077.py
@@ -1,2 +1,9 @@
 vogais = ('a','e','i','o','u')
-pergunta = str(input('Digite uma palavra: ')).strip
+pergunta = str(input('Digite uma palavra: ')).lower()
+num_vogais = 0
+vogaisl = []
+for letra in pergunta:
+    if letra in vogais:
+        num_vogais += 1
+        vogaisl.append(letra)
+print(f'A {num_vogais} vogais nessa palavra e são: {vogaisl}')
