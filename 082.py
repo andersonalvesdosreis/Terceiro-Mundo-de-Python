1b58b7907792bf9c94eb040f2afce56d7512ad7b
Anderson Alves <anderson.filho.xap@gmail.com>
2026-05-20 16:14:17 -0500

    Adicionei Resposta

--- 082.py
+++ 082.py
@@ -1,10 +1,18 @@
 lista = []
+impar = []
+par = []
 while True:
     pergunta = int(input('Digite um numero: '))
     lista.append(pergunta)
+    if pergunta %2 == 0:
+        par.append(pergunta)
+    else:
+        impar.append(pergunta)
     pergunta2 = str(input('Deseja continuar? (s/n): ')).lower()
     if pergunta2 in 's':
         continue
     else:
         break
 print(f'A lista foi: {lista}')
+print(f'Lista dos pares {par}')
+print(f'Lista dos impares {impar}')
