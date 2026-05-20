d52f87b17a54ad1687a3ac875be3834f7b49981b
Anderson Alves <anderson.filho.xap@gmail.com>
2026-05-20 14:56:51 -0500

    Adicionei resposta

---
+++ 080.py
@@ -0,0 +1,12 @@
+for add in range(0,5):
+    pergunta = int(input("digite um numero: "))
+    print(f'Adicionando {pergunta} ao final da lista...')
+    pergunta2 = int(input("digite um numero: "))
+    print(f'Adicionando {pergunta2} ao começo da lista...')
+    pergunta3 = int(input("digite um numero: "))
+    print(f'Adicionando {pergunta3} na posição 1 da lista')
+    pergunta4 = int(input("digite um numero: "))
+    print(f'Adicionando {pergunta4} ao final da lista...')
+    pergunta5 = int(input("digite um numero: "))
+    print(f'Adicionando {pergunta5} no começo da lista...')
+    
