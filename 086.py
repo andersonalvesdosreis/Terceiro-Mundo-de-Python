89acf9cb7302ba4f98baeaa6b7cc97d71bde806f
Anderson Alves <anderson.filho.xap@gmail.com>
2026-07-27 18:54:08 -0500

    Adicionei resposta!

---
+++ 086.py
@@ -0,0 +1,8 @@
+matriz = [[], [], [], [], [], [], [], [], []]
+
+for x in range(0, 9):
+    matriz[x].append(int(input('digite um valor: ')))
+
+print(matriz[0], matriz[1], matriz[2])
+print(matriz[3], matriz[4], matriz[5])
+print(matriz[6], matriz[7], matriz[8])
