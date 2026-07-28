a6d3560df90e5dc3aebdcf040ecaa27024178ba2
Anderson Alves <anderson.filho.xap@gmail.com>
2026-07-28 10:45:09 -0500

    Adicionei Resposta!

--- 088.py
+++ 088.py
@@ -1,3 +1,14 @@
+import random
 print('-'*20)
 print(' '*5,'Jogo da mega Sena',' '*5)
 print('-'*20)
+
+n = int(input('Quantos jogos você deseja? '))
+print(f'Sorteando {n} Jogos!')
+
+for num in range(1,n+1):
+    numeros = random.sample(range(1, 60), 6)
+    numeros.sort()
+    print(f'O Jogo{num}: {numeros}')
+
+print('Boa Sorte!')
