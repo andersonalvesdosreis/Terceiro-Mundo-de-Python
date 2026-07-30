b6f3fd925fe6feff6cc2a38d6af335de2372d241
Anderson Alves <anderson.filho.xap@gmail.com>
2026-07-30 18:36:38 -0500

    Adicionei Resposta!

---
+++ 096.py
@@ -0,0 +1,8 @@
+def mult(a,b):
+    s = a * b
+    print(f'A area total é de {a} x {b} = {s}')
+
+
+pergunta = int(input('Largura: '))
+pergunta2 = int(input('Comprimento: '))
+mult(pergunta,pergunta2)
