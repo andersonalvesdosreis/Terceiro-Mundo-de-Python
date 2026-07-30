9c1a4438db8715c7777395f6f71d9c09be60474e
Anderson Alves <anderson.filho.xap@gmail.com>
2026-07-30 17:01:56 -0500

    Adicionei Resposta!

---
+++ 094.py
@@ -0,0 +1,41 @@
+lista = []
+lista_mulheres = []
+contador = 0 
+idade_de_todos = 0
+
+while True:
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    idade_de_todos += idade
+    sexo = str(input('Sexo(M/F): ')) 
+    if sexo != 'M' or sexo != 'F':
+        print(f'Você digitou {sexo} que não se enquadra nas restrições tente novamente!') 
+        print('Digite novamente!')
+        sexo = str(input('Sexo(M/F): '))
+    if sexo in 'Ff':
+        lista_mulheres.append(nome)
+    discionario = {'nome':nome,'idade':idade,'sexo':sexo}
+    lista.append(discionario)
+    contador += 1
+    pergunta = str(input('Quer Continuar?(S/N) '))
+    if pergunta in 'Ss':
+        continue
+    elif pergunta in 'Nn':
+        break
+    else:
+        print(f'Você digitou {pergunta} que não se enquadra nas restrições tente novamente!') 
+        pergunta = str(input('Quer Continuar?(S/N) '))
+
+print('-=-='*15)
+
+print(f'Tivemos ao todo {contador} Cadastros!')
+
+media = (idade_de_todos) / (contador)
+
+print(f'A Média das Idades foram de {media}')
+print(f'As Mulheres cadastradas foram: {lista_mulheres}')
+
+print('As pessoas Cadastradas Acima da Média foram:')
+for analisar in lista:
+    if idade > media:
+        print(f'nome: {nome},idade: {idade},sexo: {sexo}')
