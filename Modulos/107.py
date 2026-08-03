ceb1accdca3434b710bd6cb2c071500ae0abc75c
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-03 18:52:03 -0500

    Adicionei Resposta!

--- Modulos/107.py
+++ Modulos/107.py
@@ -1,1 +1,5 @@
 import func
+pergunta = int(input('Digite um valor: '))
+print(f'A metade de R${pergunta} é {func.met(pergunta)}')
+print(f'O dobro de R${pergunta} é {func.dobro(pergunta)}')
+print(f'Aumentando 10% do valor original fica: {func.aumento(pergunta)}')
---
+++ Modulos/func.py
@@ -0,0 +1,14 @@
+def met(num):
+    metade = num/2
+    return (f'R${metade:.1f}')
+
+
+def dobro(num):
+    dobro = num*2
+    return (f'R${dobro:.1f}')
+
+
+def aumento(num):
+    novo_valor = num + ((num*10)/100)
+    return (f'R${novo_valor:.1f}')
+
