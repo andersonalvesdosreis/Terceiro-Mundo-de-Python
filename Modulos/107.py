d053e6e28c6a1e5f6b67b938c30139ed0cae0b29
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-03 18:54:58 -0500

    Ajustei, Adicionei e Reformulei as Respostas!

--- Modulos/107.py
+++ Modulos/107.py
@@ -1,5 +1,5 @@
-import func
+import func_107
 pergunta = int(input('Digite um valor: '))
-print(f'A metade de R${pergunta} é {func.met(pergunta)}')
-print(f'O dobro de R${pergunta} é {func.dobro(pergunta)}')
-print(f'Aumentando 10% do valor original fica: {func.aumento(pergunta)}')
+print(f'A metade de R${pergunta} é {func_107.met(pergunta)}')
+print(f'O dobro de R${pergunta} é {func_107.dobro(pergunta)}')
+print(f'Aumentando 10% do valor original fica: {func_107.aumento(pergunta)}')
