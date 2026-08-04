df6e2e2a3cdb693e63bfd0cf8853fc613d377186
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-04 14:17:31 -0500

    Adicionei Resposta!

---
+++ Modulos/111.py
@@ -0,0 +1,7 @@
+from func_110 import resumo
+pergunta = int(input('Digite um valor: R$'))
+resumo(pergunta)
+#A unica mudança solicitada foi a criação de um 
+# pacote invez do uso de modulo porem 
+# não sera possivel realizar devido ao 
+# tamanho do arquivo usado durante as minhas resoluções!
