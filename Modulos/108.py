d22c6f1770e7a2c086beaf862a8855429f507e7b
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-03 19:04:38 -0500

    Adicionei Resposta!

---
+++ Modulos/108.py
@@ -0,0 +1,5 @@
+import func_108
+pergunta = int(input('Digite um valor: '))
+print(f'A metade de R${func_108.novo_valor(pergunta)} é {func_108.met(pergunta)}')
+print(f'O dobro de R${func_108.novo_valor(pergunta)} é {func_108.dobro(pergunta)}')
+print(f'Aumentando 10% do valor original fica: {func_108.aumento(pergunta)}')
--- Modulos/func_108.py
+++ Modulos/func_108.py
@@ -1,14 +1,17 @@
 def met(num):
     metade = num/2
-    return (f'R${metade:.1f}')
+    return (f'R${metade:.2f}'.replace(".", ","))
 
 
 def dobro(num):
     dobro = num*2
-    return (f'R${dobro:.1f}')
+    return (f'R${dobro:.2f}'.replace(".", ","))
 
 
 def aumento(num):
     novo_valor = num + ((num*10)/100)
-    return (f'R${novo_valor:.1f}')
+    return (f'R${novo_valor:.2f}'.replace(".", ","))
 
+
+def novo_valor(num):
+    return (f'{num:.2f}'.replace(".", ","))
