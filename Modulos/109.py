5ded50cc536a36ad6085bcc9cbfd1e5407296da3
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-03 19:13:37 -0500

    Adicionei Resposta!

---
+++ Modulos/109.py
@@ -0,0 +1,6 @@
+import func_108
+pergunta = int(input('Digite um valor: R$'))
+print(f'A metade de R${func_108.novo_valor(pergunta)} é {func_108.met(pergunta)}')
+print(f'O dobro de R${func_108.novo_valor(pergunta)} é {func_108.dobro(pergunta)}')
+print(f'Aumentando 10% do valor original fica: {func_108.aumento(pergunta)}')
+#Codigo copiado devido a inexistencia de nenhum exesso de parametro!
